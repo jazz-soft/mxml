696a2a10b633a28633e1d606cc4f5e489d8efa3e
Sema <jazz-dev@jazz-soft.net>
2024-10-23 22:41:24 -0400

    Specs from musicxml.xsd

--- mxml.mjs
+++ mxml.mjs
@@ -1,18 +1,32 @@
 import {EditorView, basicSetup} from "codemirror"
-//import {xmlLanguage} from "@codemirror/lang-xml"
+import {syntaxTree} from '@codemirror/language';
 import {xml} from "@codemirror/lang-xml"
+import specs from "./specs.cjs"
 
 function MxmlEditor(where) {
   this.editor = new EditorView({
-  extensions: [basicSetup, xml()],
+    extensions: [basicSetup, xml({elements: specs.elements})],
     parent: document.getElementById(where)
   });
   this.setText = function(txt) {
-    this.editor.dispatch(this.editor.state.update({changes: {
-    from: 0, to: this.editor.state.doc.length, insert: txt}, selection: {anchor: 0}
-  }));
-};
-
+    this.editor.dispatch(this.editor.state.update({
+      changes: { from: 0, to: this.editor.state.doc.length, insert: txt },
+      selection: { anchor: 0 }
+    }));
+  };
+  this.getText = function() {
+    return this.editor.state.doc.toString();
+  };
+  this.test = function() {
+    var tree = syntaxTree(this.editor.state);
+    if (!tree) { console.log('No tree'); return; }
+    var cursor = tree.cursor();
+    var txt = this.getText();
+    while (true) {
+      console.log(cursor.name, cursor.from, cursor.to, txt.substring(cursor.from, cursor.to).substring(0, 80));
+      if (!cursor.next()) break;
+    }
+  };
 }
 
 window.MxmlEditor = MxmlEditor;
--- specs/specs.js
+++ specs/specs.js
@@ -1,18 +1,66 @@
 const fs = require('fs');
-const {XMLParser, XMLBuilder} = require('fast-xml-parser');
+const {XMLParser} = require('fast-xml-parser');
 
-var i;
+var i, k, A;
 var body = [];
-body.push("const p2t = '" + read_xml('parttime.xsl') + "';");
-body.push("const t2p = '" + read_xml('timepart.xsl') + "';");
+var Elem = {};
+var Attr = {};
+var AttrG = {};
+
+const parser = new XMLParser({ ignoreAttributes: false });
+const xml = parser.parse(fs.readFileSync('musicxml.xsd', 'utf8'));
+
+traverse(xml, function(x) { delete x['xs:annotation']});
+traverse(xml, collect('xs:element', Elem));
+traverse(xml, collect('xs:attribute', Attr));
+traverse(xml, collect('xs:attributeGroup', AttrG));
+
+body.push('const elements = [');
+A = [];
+for (k of Object.keys(Elem)) A.push(make_elem(k));
+body.push(A.join(',\n'));
+body.push('];');
+
+body.push('const attributes = [');
+A = [];
+for (k of Object.keys(Attr)) A.push(make_attr(k));
+body.push(A.join(',\n'));
+body.push('];');
 
 const lines = fs.readFileSync('specs.txt', 'utf8').split(/\r?\n/);
 for (i = 0; i < lines.length; i++) if (lines[i] == '<BODY>') lines[i] = body.join('\n');
+//console.log(lines.join('\n'));
 fs.writeFileSync('../specs.cjs', lines.join('\n'), 'utf8');
 
-function read_xml(fname) {
-  const data = fs.readFileSync(fname, 'utf8');
-  const parser = new XMLParser({ ignoreAttributes: false });
-  const builder = new XMLBuilder({ ignoreAttributes: false });
-  return builder.build(parser.parse(data));
+function traverse(x, f) {
+  if (Array.isArray(x)) for (var y of x) traverse(y, f);
+  else if (typeof x == 'object') {
+    f(x);
+    for (var k of Object.keys(x)) traverse(x[k], f);
+  }
+}
+function collect(name, arr) {
+  function fun(x) {
+    var name = x['@_name'];
+    if (!name) return;
+    if (!arr[name]) arr[name] = [];
+    arr[name].push(x);
+  }
+  return function(x) {
+    var y = x[name];
+    if (Array.isArray(y)) for (var z of y) fun(z);
+    else if (typeof y == 'object') fun(y);
+  };
+}
+
+function make_elem(k) {
+  var x = Elem[k][0];
+  var s ='{name:"' + k + '"';
+  if (x['@_final']) s += ',top:true';
+  s += '}';
+  return s;
+}
+function make_attr(k) {
+  //var x = Attr[k];
+  return '{name:"' + k + '"}'
 }
