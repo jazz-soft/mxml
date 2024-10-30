eb6a469a4246d9374f37908efd146051f8aa3f42
Sema <jazz-dev@jazz-soft.net>
2024-10-29 22:49:03 -0400

    Build script

---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+import nodejs from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'mxml.mjs',
+  output: {
+    file: 'mxml.js',
+    format: 'iife'
+  },
+  plugins: [nodejs(), commonjs(), skip('zlib')]
+};
+
+function skip(...args) {
+  return {
+    name: 'skip',
+    resolveId(x) {
+      for (var a of args) if (a == x) return x;
+      return null;
+    },
+    load(x) {
+      for (var a of args) if (a == x) return 'export default {}';
+      return null;
+    }
+  };
+}
